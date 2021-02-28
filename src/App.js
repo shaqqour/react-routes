ed993d73a2f34812eb74e7822267f8bf4a3d7479
Shaqqour <mshaqqourm@gmail.com>
2021-02-28 13:45:32 -0800

    Adds the routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,52 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import NavBar from './components/NavBar';
+import Home from './components/Home';
+import Songs from './components/Songs';
+import Artists from './components/Artists';
+import Writers from './components/Writers';
 
 function App() {
   return (
     <div>
-        <img src={logo} className="App-logo" alt="logo" />
         <Router>
           <NavBar />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/artists" component={Artists} />
+          <Route exact path="/writers" component={Writers} />
+          <Route exact path="/songs" component={Songs} />
         </Router>
     </div>
   );
 }
 
 export default App;
-
-
-
-
-
-
-
-
-
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
