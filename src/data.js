40fdd96366ca1dc2908ad211262fa856e8dcaaf9
Shaqqour <mshaqqourm@gmail.com>
2021-02-28 08:20:08 -0800

    Creates data.js to store some data to be used in the application

---
+++ src/data.js
@@ -0,0 +1,46 @@
+let songs = [
+    {
+        title: 'No Woman, No Cry',
+        genres: ['Romance', 'Fantasy'],
+    },
+    {
+        title: 'Is This Love',
+        genres: ['Romance', 'Comedy']
+    },
+    {
+        title: 'Ya Agmal 3oyoon',
+        genres: ['Romance', 'Fantasy']
+    }
+]
+
+let artists = [
+    {
+        name: 'Bob Marley',
+        songs: ['No Woman, No Cry', 'Is This Love']
+    },
+    {
+        name: 'Amr Diab',
+        songs: ['Ya Agmal 3oyoon', 'Burg Al 7oot']
+    }
+]
+
+let writers = [
+    {
+        name: 'Vincent Ford',
+        songs: ['No Woman, No Cry', 'Crazy Baldhead']
+    },
+    {
+        name: 'Bob Marley',
+        movies: ['In This Love']
+    },
+    {
+        name: 'Turky Al Sheekh',
+        movies: ['Ya Agmal 3oyoon', 'A7la 7aga']
+    }
+]
+
+module.exports = {
+    songs,
+    artists,
+    writers
+}
